d731a65976e52727ece0bcccf6f41ba603576651
Bimal27 <bimalsah1996@gmail.com>
2023-02-03 20:32:37 +0100

    chat application using react-chat-application

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ChatEngine } from "react-chat-engine";
+import "./App.css";
+import Login from "./LoginForm";
+
+const projectID = "75c73eb0-0f50-4536-bb4f-896082b1a58f";
 
 function App() {
+  if (!localStorage.getItem("username")) return <Login />;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem("username")}
+      userSecret={localStorage.getItem("password")}
+    />
   );
 }
 
